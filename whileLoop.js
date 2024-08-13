c93f0520eece7d125c903a7e023e082cce6d03cd
Atikur Rahman <atikdev.bd@gmail.com>
2024-08-13 12:37:57 +0600

    four step to complete while loop

--- whileLoop.js
+++ whileLoop.js
@@ -6,3 +6,10 @@ while (rost < people) {
   rost++;
   console.log(rost);
 }
+
+// 4 step to compleate while loop ///
+
+// 1 : loop variable  exp: var rost = 0;
+// 2: loop condition  exp: while (rost < people)
+// 3: loop body      exp: console.log("give me a rost ")
+// 4: loop change     exp:   rost++
