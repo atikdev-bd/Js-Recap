1c55c467dee432514fb08c6ed927c0abc8f2da99
Atikur Rahman <atikdev.bd@gmail.com>
2024-08-13 09:57:06 +0600

    recap javaScript index method

---
+++ index.js
@@ -0,0 +1,15 @@
+var numbers = [55, 66, 44, 77, 68];
+
+// \\\ get Element value by array
+
+var element = numbers[4];
+
+// set element vale by index
+numbers[3] = 89;
+// find index of an element
+
+var findIndex = numbers.indexOf(89);
+
+numbers[3] = 89;
+
+console.log(findIndex);
