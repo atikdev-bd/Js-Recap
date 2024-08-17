71607b2ca9f37704a87441fd017d0441a09510bd
Atikur Rahman <atikdev.bd@gmail.com>
2024-08-17 14:34:05 +0600

    string array loop recap

--- arrayLoop.js
+++ arrayLoop.js
@@ -1,7 +1,14 @@
 var numbers = [44, 66, 77, 88, 99, 45];
 
-for (var i = 0; i < 6; i++) {
+for (var i = 0; i < numbers.length; i++) {
   let number = numbers[i];
 
-  console.log(i, number)
+  console.log(i, number);
+}
+
+var items = ["pen", "bottle", "pencle", "eirPhone"];
+
+for (let i = 0; i < items.length; i++) {
+  let item = items[i];
+  console.log(item);
 }
